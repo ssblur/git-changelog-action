565c1b25be55f0fc0bba5b8325f413aab51e9ba0
Patrick Hallbick <patrick.hallbick@outlook.com>
2023-04-05 21:20:26 -0400

    Put output in changelog
    Signed-off-by: Patrick Hallbick <patrick.hallbick@outlook.com>

--- action.sh
+++ action.sh
@@ -1,2 +1,5 @@
 TAG=$(git tag | tail -2 | head -1)
+EOF=$(dd if=/dev/urandom bs=15 count=1 status=none | base64)
+echo "changelog<<$EOF" >> $GITHUB_ENV
 git log --no-merges --pretty=tformat:%s HEAD...$TAG >> $GITHUB_OUTPUT
+echo "$EOF" >> $GITHUB_OUTPUT
