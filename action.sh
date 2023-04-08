0425d64f6b6098b0530d79bef15453e06f98dcc1
Patrick Hallbick <patrick.hallbick@outlook.com>
2023-04-08 00:02:06 -0400

    Fix typo
    Signed-off-by: Patrick Hallbick <patrick.hallbick@outlook.com>

--- action.sh
+++ action.sh
@@ -1,5 +1,5 @@
 TAG=$(git tag | tail -2 | head -1)
 EOF=$(dd if=/dev/urandom bs=15 count=1 status=none | base64)
-echo "changelog<<$EOF" >> $GITHUB_ENV
+echo "changelog<<$EOF" >> $GITHUB_OUTPUT
 git log --no-merges --pretty=tformat:%s HEAD...$TAG >> $GITHUB_OUTPUT
 echo "$EOF" >> $GITHUB_OUTPUT
